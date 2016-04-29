3d00516390f3a7fc46e92f736730c6803d9824de
Marco <france.marco@gmail.com>
2016-04-29 23:56:54 +0200

    Casino per cose

---
+++ Assets/BackManagement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackManagement : MonoBehaviour {
+
+	public float limit = 32;
+	public Vector2 pos;
+	public Object prefab;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2(0, limit * 2);
+		transform.SetParent(transform.parent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = transform.position;
+		if (pos.y < -limit) {
+			GameObject obj=(GameObject)Instantiate(prefab);
+			///obj.transform.SetParent(transform.parent);
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Programming/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float XSpeed = 0;
+	public float YSpeed = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector2(XSpeed, YSpeed) * Time.deltaTime);
+	}
+}
