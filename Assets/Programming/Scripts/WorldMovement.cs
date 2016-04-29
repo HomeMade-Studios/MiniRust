246964276bb008744eeca03e498d439a2bebdc9b
Marco <france.marco@gmail.com>
2016-04-29 19:19:55 +0200

    Anim

---
+++ Assets/Programming/Scripts/WorldMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldMovement : MonoBehaviour {
+
+	public float WorldSpeed = 25;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector2(0, -WorldSpeed) * Time.deltaTime);
+	}
+}
