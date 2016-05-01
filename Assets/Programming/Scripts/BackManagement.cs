17613665fba6f72212f9fc6fab88139758995c24
Marco <france.marco@gmail.com>
2016-05-01 23:28:27 +0200

    sthbsgfj

--- Assets/Programming/Scripts/BackManagement.cs
+++ Assets/Programming/Scripts/BackManagement.cs
@@ -9,15 +9,18 @@ public class BackManagement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		//transform.position = new Vector2(transform.position.x, Mathf.Round(transform.position.y));
+		int yPos = Mathf.FloorToInt(transform.localPosition.y / 512) * 512;
+        transform.localPosition = new Vector2(transform.localPosition.x, yPos);
+		this.name = "Background_" + ((transform.localPosition.y / 512));
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		pos = transform.position;
 		if (pos.y < -limit) {
-			GameObject obj=(GameObject)Instantiate(prefab,new Vector3(0, limit * 2, 0),Quaternion.Euler(Vector3.zero));
-			obj.transform.SetParent(transform.parent);
+			GameObject obj=(GameObject)Instantiate(this.gameObject,new Vector3(0, limit * 2, 0),Quaternion.identity);
+			obj.transform.parent=transform.parent;
 			obj.transform.position =new Vector2(0, Mathf.Round(limit * 2));
 			Destroy(this.gameObject);
 		}
---
+++ Assets/Programming/Scripts/BulletHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletHandler : MonoBehaviour {
+
+	public float speed;
+	public Vector2 direction;
+	float sin;
+	float cos;
+	
+
+	// Use this for initialization
+	void Start () {
+		sin = Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.z);
+		cos = Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.z);
+		direction = new Vector2(sin, cos);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( direction*speed * Time.deltaTime);
+		Debug.Log(direction * speed);
+	}
+}
